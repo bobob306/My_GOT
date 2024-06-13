a56e57ea25954b24eef8c83a17b53a4f4b42f2e6
Ben Shapiro <ben.shapiro@clearscore.com>
2024-06-13 13:01:00 +0100

    add a cancel to the timer

--- app/src/main/java/com/bensdevelops/myGOT/ui/screens/dummyScreen/DummyScreen.kt
+++ app/src/main/java/com/bensdevelops/myGOT/ui/screens/dummyScreen/DummyScreen.kt
@@ -5,10 +5,17 @@ import android.os.Vibrator
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,7 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
@@ -24,8 +31,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.bensdevelops.myGOT.ui.theme.SizeTokens
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -35,38 +46,87 @@ fun DummyScreen(
 ) {
     val context = LocalContext.current
     val vibrator = context.getSystemService(Vibrator::class.java)
+    val viv = viewModel.viv.collectAsState()
     val number by viewModel.number.collectAsState()
+    val loading by viewModel.loading.collectAsState()
+    val scope = rememberCoroutineScope()
+    LaunchedEffect(key1 = viv.value) {
+        if (!viv.value) return@LaunchedEffect
+        Log.d("vib", "ration started now")
 
-    Surface(Modifier.fillMaxSize()) {
+        val effect =
+            VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE)
+        vibrator.vibrate(effect)
+        viewModel.resetViv()
+    }
+    if (loading) {
+        val initialCount = number
+        var count by remember {
+            mutableIntStateOf(initialCount)
+        }
+        LaunchedEffect(key1 = Unit) {
+            while (count > 0) {
+                delay(1000)
+                count--
+            }
 
-        // Creating a Centered Column for the Vibration Buttons
-        Column(
+        }
+        Box(
+            contentAlignment = Alignment.Center,
             modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
         ) {
-            Text(text = "Enter your desired time")
-            OutlinedTextField(
-                label = { Text(text = "Enter desired time") },
-                value = number.toString(),
-                onValueChange = viewModel::handleValueChange,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .fillMaxWidth(0.75f)
+                    .aspectRatio(1f)
+                    .align(Alignment.Center)
             )
-            // Vibration Button - OneShot
-            if (number > 0) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = count.toString(), fontSize = 30.sp)
                 Button(onClick = {
-                    // Safely cancel any ongoing vibrations
-                    vibrator.cancel()
-                    viewModel.onClick()
-                    // Handling vibrations for Android 8.0 (Oreo) and above
-                    Log.d("vib", "ration started now")
-                    val effect =
-                        VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE)
-                    vibrator.vibrate(effect)
+                    viewModel.onClearClick()
+                }) {
+                    Text(text = "Cancel timer")
                 }
+            }
 
-                ) {
-                    Text(text = "Vibration after $number seconds")
+
+        }
+    } else {
+
+        Surface(Modifier.fillMaxSize()) {
+            // Creating a Centered Column for the Vibration Buttons
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Enter your desired time")
+                OutlinedTextField(
+                    label = { Text(text = "Enter desired time") },
+                    value = number.toString(),
+                    onValueChange = viewModel::handleValueChange,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                )
+                Spacer(modifier = Modifier.size(SizeTokens.medium))
+                // Vibration Button - OneShot
+                if (number > 0) {
+                    Button(
+                        shape = CircleShape,
+                        modifier = Modifier
+                            .fillMaxWidth(0.75f)
+                            .aspectRatio(1f),
+                        onClick = {
+                            // Safely cancel any ongoing vibrations
+                            vibrator.cancel()
+                            scope.launch { viewModel.onClick() }.invokeOnCompletion {
+                                // Handling vibrations for Android 8.0 (Oreo) and above
+                            }
+                        },
+
+                        ) {
+                        Text(text = "Vibration after $number seconds")
+                    }
                 }
             }
         }
--- app/src/main/java/com/bensdevelops/myGOT/ui/screens/dummyScreen/DummyScreenViewModel.kt
+++ app/src/main/java/com/bensdevelops/myGOT/ui/screens/dummyScreen/DummyScreenViewModel.kt
@@ -1,33 +1,55 @@
 package com.bensdevelops.myGOT.ui.screens.dummyScreen
 
 import android.util.Log
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.bensdevelops.myGOT.core.base.ViewData
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import java.lang.Thread.sleep
 import javax.inject.Inject
 
 class DummyScreenViewModel @Inject constructor() : ViewModel() {
     private var _number = MutableStateFlow(0)
     val number = _number.asStateFlow()
 
+    private var _viv = MutableStateFlow(false)
+    val viv = _viv.asStateFlow()
+
+    private var _loading = MutableStateFlow(false)
+    val loading = _loading.asStateFlow()
+
+    private var currentJob: Job? = null
+
     fun handleValueChange(newText: String) {
         val newValue = if (newText == "0") 0 else newText.filter { it.isDigit() }.toIntOrNull() ?: 0
         _number.value = newValue
     }
 
-    fun onClick() {
+    fun resetViv() {
+        _viv.value = false
+    }
+
+    suspend fun onClick() {
+        _loading.value = true
         Log.d("vib", "ration waiting begins")
-        sleep(number.value.times(1000).toLong())
-        Log.d("vib", "ration waiting ends")
+        countDown()
+    }
+
+    private suspend fun countDown(cancel: Boolean? = false) {
+        currentJob = viewModelScope.launch {
+                delay(number.value.times(1000).toLong())
+                Log.d("vib", "ration waiting ends")
+                _viv.value = true
+                _loading.value = false
+        }
+    }
+
+    fun onClearClick() {
+        _number.value = 0
+        currentJob?.cancel()
+        _viv.value = false
+        _loading.value = false
     }
 }
