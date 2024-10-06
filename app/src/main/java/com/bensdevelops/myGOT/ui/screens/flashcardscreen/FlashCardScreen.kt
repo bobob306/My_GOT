0d6303a867cd8b343206b35b27db510371e3f30c
bobob306 <94963479+bobob306@users.noreply.github.com>
2024-10-06 14:04:44 +0100

    add flash card screen

--- app/src/main/java/com/bensdevelops/myGOT/ui/screens/flashcardscreen/FlashCardScreen.kt
+++ app/src/main/java/com/bensdevelops/myGOT/ui/screens/flashcardscreen/FlashCardScreen.kt
@@ -4,12 +4,17 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +26,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
@@ -145,12 +151,7 @@ fun FlashCard(
                 } else {
                     // Apply rotation to the back content
                     Column(modifier = Modifier.graphicsLayer { rotationY = 180f }) {
-                        AnswerContent(answer = viewData.answer)
-                        Button(onClick = {
-                            onNextQuestionClick()
-                        }, modifier = Modifier.padding(16.dp)) {
-                            Text(text = "Next Question")
-                        }
+                        AnswerContent(answer = viewData.answer, onNextQuestionClick)
                     }
                 }
             }
@@ -172,14 +173,16 @@ fun FlashCard(
 
 @Composable
 fun QuestionContent(question: String) {
-    Box(
+    Card(
         modifier = Modifier
-            .fillMaxWidth()
             .padding(16.dp)
             .background(MaterialTheme.colorScheme.primary)
+            .fillMaxWidth()
     ) {
         Text(
-            modifier = Modifier.padding(16.dp),
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(16.dp),
             text = question,
             style = MaterialTheme.typography.headlineSmall,
             color = MaterialTheme.colorScheme.onPrimary,
@@ -188,18 +191,39 @@ fun QuestionContent(question: String) {
 }
 
 @Composable
-fun AnswerContent(answer: String) {
-    Box(
+fun AnswerContent(answer: String, onNextQuestionClick: () -> Unit) {
+    Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-            .background(MaterialTheme.colorScheme.primary)
+            .fillMaxSize()
+            .padding(vertical = 8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(
-            modifier = Modifier.padding(16.dp),
-            text = answer,
-            style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onPrimary
-        )
+        Card(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .weight(0.9f)
+        ) {
+            Text(
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.primary),
+                text = answer,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onPrimary,
+            )
+        }
+        Button(
+            onClick = {
+                onNextQuestionClick()
+            }, modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .fillMaxWidth()
+                .wrapContentSize()
+        ) {
+            Text(text = "Next Question")
+        }
     }
 }
